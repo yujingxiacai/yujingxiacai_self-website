e4a7bdb713394c7defa7cac010409b010edc4363
shenyukang <2539713003@qq.com>
2018-11-29 22:46:02 +0800

    关于hash值的测试

--- yu-site/src/router.js
+++ yu-site/src/router.js
@@ -7,7 +7,7 @@ import Contact from '@/components/Contact.vue';
 
 Vue.use(Router);
 export default new Router({
-  mode: 'history',
+  mode: 'hash',
   base: process.env.BASE_URL,
   routes: [
     {
